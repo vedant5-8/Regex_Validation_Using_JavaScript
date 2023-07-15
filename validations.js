abd8fc0d6fb50d789de2b0bc70f20dbedb52be59
Vedant <vedantnp200@gmail.com>
2023-07-16 00:38:45 +0530

    [Vedant]-The Postal Index Number

---
+++ validations.js
@@ -0,0 +1,10 @@
+let PinCode = 400088;
+
+let PinCodeRE = RegExp('^[1-9][0-9]{5}$');
+
+if (PinCodeRE.test(PinCode)) {
+    console.log("Valid Pin Code");
+}
+else {
+    console.log("Invalid Pin Code");
+}
