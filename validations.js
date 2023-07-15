1df84aaa484005b36b142e6e08d1d28a4b31a7f1
Vedant <vedantnp200@gmail.com>
2023-07-16 00:43:58 +0530

    [Vedant]-The Postal Index Number (PIN) or PIN Code is a 6 digit code of Post Office numbering used by India Post.

--- validations.js
+++ validations.js
@@ -1,3 +1,5 @@
+// UC 1 - The Postal Index Number (PIN) or PIN Code is a 6 digit code of Post Office numbering used by India Post."
+
 let PinCode = 400088;
 
 let PinCodeRE = RegExp('^[1-9][0-9]{5}$');
