e98946f2fca331dcb12c087d8b92ffc58f888c67
Vedant <vedantnp200@gmail.com>
2023-07-16 00:40:20 +0530

    [Vedant]-The Postal Index Number (PIN) or PIN Code is a 6 digit code of Post Office numbering used by India Post.

--- validations.js
+++ validations.js
@@ -3,8 +3,8 @@ let PinCode = 400088;
 let PinCodeRE = RegExp('^[1-9][0-9]{5}$');
 
 if (PinCodeRE.test(PinCode)) {
-    console.log("Valid Pin Code");
+    console.log("Valid Pin Code.");
 }
 else {
-    console.log("Invalid Pin Code");
+    console.log("Invalid Pin Code.");
 }
