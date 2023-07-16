59e0384405a111c5682c1b04999c2c8a87fcf776
Vedant <vedantnp200@gmail.com>
2023-07-16 16:39:54 +0530

    [Vedant]-Validate Email address with a regex.

---
+++ Email_Validations.js
@@ -0,0 +1,12 @@
+// UC 1 - Validate Email address with a regex.
+
+let email = 'abc.xyz@bridgelabz.co.in';
+
+let EmailRE = RegExp('^[0-9a-zA-Z]+[+._-]{0,1}[0-9a-zA-Z]+[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$');
+
+if (EmailRE.test(email)) {
+    console.log(email + " is valid email.");
+}
+else {
+    console.log(email + " is invalid email.");
+}
